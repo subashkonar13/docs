b37132aa9c4b7e7b05048d374185ceb816924eb7
subashkonar13 <subashkonar13@gmail.com>
2024-05-30 02:59:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openTab(tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
